cfef8f3c2d7dbcdf207106b7bac7b0f875cf011e
Letruk190 <Letruk190@yandex.ru>
2022-01-14 18:35:04 +0300

    new

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,12 +1,19 @@
 package com.gridnine.testing;
 
+import com.gridnine.testing.moduleSearch.ArriveBeforeDeparture;
+import com.gridnine.testing.moduleSearch.FlightBeforeNow;
+import com.gridnine.testing.moduleSearch.MoreTwoHoursWait;
+import com.gridnine.testing.testclass.Flight;
 import com.gridnine.testing.testclass.FlightBuilder;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(FlightBuilder.createFlights());
+        List<Flight> flights = FlightBuilder.createFlights();
+        System.out.println(new FlightBeforeNow().getFilteredFlights(flights));
+        System.out.println(new ArriveBeforeDeparture().getFilteredFlights(flights));
+        System.out.println(new MoreTwoHoursWait().getFilteredFlights(flights));
 
 
     }
